70519904ececa5885f8f833600c55397f8bf07cc
engrjeff <engineerjeff15@gmail.com>
2025-09-22 12:35:45 +0800

    add quick actions

--- app/(u)/dashboard/page.tsx
+++ app/(u)/dashboard/page.tsx
@@ -1,4 +1,5 @@
 import type { Metadata } from "next";
+import { QuickActions } from "@/components/quick-actions";
 import { CellGroupStatistics } from "@/features/reports/cell-group-statistics";
 import { MemberStatistics } from "@/features/reports/member-statistics";
 import { RecentCellGroups } from "@/features/reports/recent-cell-groups";
@@ -10,12 +11,19 @@ export const metadata: Metadata = {
 export default function DashboardPage() {
   return (
     <div className="space-y-4 p-4">
-      <div>
-        <h2 className="font-bold">Dashboard</h2>
-        <p className="text-sm text-muted-foreground">
-          Quick insights on cell groups and members.
-        </p>
+      <div className="flex items-center justify-between gap-4">
+        <div>
+          <h2 className="font-bold">Dashboard</h2>
+          <p className="text-sm hidden md:block text-muted-foreground">
+            Quick insights on cell groups and members.
+          </p>
+          <p className="text-sm md:hidden text-muted-foreground">
+            Quick cell group insights.
+          </p>
+        </div>
+        <QuickActions />
       </div>
+
       <CellGroupStatistics />
       <RecentCellGroups />
       <MemberStatistics />
---
+++ components/quick-actions.tsx
@@ -0,0 +1,194 @@
+"use client";
+
+import {
+  ChevronDownIcon,
+  InfoIcon,
+  PlusIcon,
+  UserPlusIcon,
+} from "lucide-react";
+import { useState } from "react";
+import { Button } from "@/components/ui/button";
+import {
+  Drawer,
+  DrawerContent,
+  DrawerDescription,
+  DrawerFooter,
+  DrawerHeader,
+  DrawerTitle,
+  DrawerTrigger,
+} from "@/components/ui/drawer";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+import {
+  Sheet,
+  SheetContent,
+  SheetDescription,
+  SheetHeader,
+  SheetTitle,
+} from "@/components/ui/sheet";
+import { CellReportForm } from "@/features/cell-reports/cell-report-form";
+import { DiscipleForm } from "@/features/disciples/disciple-form";
+import { useIsMobile } from "@/hooks/use-mobile";
+
+type QuickActionType = "add-disciple" | "create-cell-report";
+
+export function QuickActions() {
+  const isMobile = useIsMobile();
+
+  const [open, setOpen] = useState(false);
+
+  const [action, setAction] = useState<QuickActionType>();
+
+  function reset() {
+    setAction(undefined);
+    setOpen(false);
+  }
+
+  if (isMobile)
+    return (
+      <>
+        <Drawer open={open} onOpenChange={setOpen}>
+          <DrawerTrigger asChild>
+            <Button size="sm">
+              <PlusIcon /> Create <ChevronDownIcon />
+            </Button>
+          </DrawerTrigger>
+          <DrawerContent>
+            <DrawerHeader className="!text-left border-b">
+              <DrawerTitle className="text-sm">Create Actions</DrawerTitle>
+              <DrawerDescription>Pick an action to do.</DrawerDescription>
+            </DrawerHeader>
+            <div className="px-0.5 py-2 flex flex-col gap-1">
+              <Button
+                size="lg"
+                variant="ghost"
+                className="w-full justify-start"
+                onClick={() => setAction("add-disciple")}
+              >
+                <UserPlusIcon /> Add a Disciple
+              </Button>
+              <Button
+                size="lg"
+                variant="ghost"
+                className="w-full justify-start"
+                onClick={() => setAction("create-cell-report")}
+              >
+                <PlusIcon /> Create Cell Report
+              </Button>
+            </div>
+            <DrawerFooter className="border-t text-xs text-muted-foreground gap-2 flex-row">
+              <InfoIcon className="size-3" />
+              <p>
+                You can also add Disciple and create Cell Report in their
+                respective pages.
+              </p>
+            </DrawerFooter>
+          </DrawerContent>
+        </Drawer>
+        <AddDiscipleForm
+          open={action === "add-disciple"}
+          setOpen={(isOpen) => {
+            if (!isOpen) {
+              reset();
+            }
+          }}
+        />
+        <CreateCellReportForm
+          open={action === "create-cell-report"}
+          setOpen={(isOpen) => {
+            if (!isOpen) {
+              reset();
+            }
+          }}
+        />
+      </>
+    );
+
+  return (
+    <>
+      <DropdownMenu>
+        <DropdownMenuTrigger asChild>
+          <Button size="sm">
+            <PlusIcon /> Create <ChevronDownIcon />
+          </Button>
+        </DropdownMenuTrigger>
+        <DropdownMenuContent align="end">
+          <DropdownMenuItem onClick={() => setAction("add-disciple")}>
+            <UserPlusIcon /> Add a Disciple
+          </DropdownMenuItem>
+          <DropdownMenuItem onClick={() => setAction("create-cell-report")}>
+            <PlusIcon /> Create Cell Report
+          </DropdownMenuItem>
+        </DropdownMenuContent>
+      </DropdownMenu>
+      <AddDiscipleForm
+        open={action === "add-disciple"}
+        setOpen={(isOpen) => {
+          if (!isOpen) {
+            reset();
+          }
+        }}
+      />
+      <CreateCellReportForm
+        open={action === "create-cell-report"}
+        setOpen={(isOpen) => {
+          if (!isOpen) {
+            reset();
+          }
+        }}
+      />
+    </>
+  );
+}
+
+function AddDiscipleForm({
+  open,
+  setOpen,
+}: {
+  open: boolean;
+  setOpen: (open: boolean) => void;
+}) {
+  return (
+    <Sheet open={open} onOpenChange={setOpen}>
+      <SheetContent
+        side="right"
+        className="inset-y-2 right-2 flex h-auto w-[95%] flex-col gap-0 overflow-y-hidden rounded-lg border bg-background p-0 focus-visible:outline-none sm:max-w-lg"
+        onInteractOutside={(e) => e.preventDefault()}
+      >
+        <SheetHeader className="border-b p-4 text-left">
+          <SheetTitle>Add Disciple</SheetTitle>
+          <SheetDescription>Fill in the details below.</SheetDescription>
+        </SheetHeader>
+        <DiscipleForm onAfterSave={() => setOpen(false)} />
+      </SheetContent>
+    </Sheet>
+  );
+}
+
+function CreateCellReportForm({
+  open,
+  setOpen,
+}: {
+  open: boolean;
+  setOpen: (open: boolean) => void;
+}) {
+  return (
+    <Sheet open={open} onOpenChange={setOpen}>
+      <SheetContent
+        side="right"
+        className="inset-y-2 right-2 flex h-auto w-[95%] flex-col gap-0 overflow-y-hidden rounded-lg border bg-background p-0 focus-visible:outline-none sm:max-w-lg"
+        onInteractOutside={(e) => e.preventDefault()}
+      >
+        <SheetHeader className="space-y-1 border-b p-4 text-left">
+          <SheetTitle>Create Cell Report</SheetTitle>
+          <SheetDescription>Fill in the details below.</SheetDescription>
+        </SheetHeader>
+        <CellReportForm onAfterSave={() => setOpen(false)} />
+      </SheetContent>
+    </Sheet>
+  );
+}

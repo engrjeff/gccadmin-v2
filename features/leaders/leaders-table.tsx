10b488dac0fd91b651d459ad323def693a5fefec
engrjeff <engineerjeff15@gmail.com>
2025-09-22 21:44:23 +0800

    add empty view

--- features/leaders/leaders-table.tsx
+++ features/leaders/leaders-table.tsx
@@ -1,4 +1,4 @@
-import { CheckIcon, XIcon } from "lucide-react";
+import { CheckIcon, PackageIcon, XIcon } from "lucide-react";
 import Link from "next/link";
 import pluralize from "pluralize";
 import type { Disciple, User } from "@/app/generated/prisma";
@@ -36,59 +36,72 @@ export function LeadersTable({
           </TableRow>
         </TableHeader>
         <TableBody>
-          {leaders.map((leader, index) => (
-            <TableRow key={leader.id} className="hover:bg-transparent">
-              <TableCell>{index + 1}</TableCell>
-              <TableCell className="font-medium">
-                <div>
-                  <Link
-                    href={`/leaders/${leader.id}`}
-                    className="hover:underline"
-                  >
-                    {leader.name}
-                  </Link>
-                  <p className="text-xs text-muted-foreground capitalize">
-                    {removeUnderscores(leader.memberType)},{" "}
-                    {leader.gender.toLowerCase()}
+          {leaders.length === 0 ? (
+            <TableRow className="hover:bg-transparent">
+              <TableCell colSpan={6}>
+                <div className="min-h-[300px] flex flex-col items-center justify-center gap-3">
+                  <PackageIcon className="size-6 text-muted-foreground" />
+                  <p className="text-sm text-muted-foreground text-center">
+                    No leader found.
                   </p>
                 </div>
               </TableCell>
-              <TableCell>
-                {leader.userAccount ? (
-                  leader.userAccount.email
-                ) : (
-                  <span className="text-muted-foreground">
-                    No account linked yet.
-                  </span>
-                )}
-              </TableCell>
-              <TableCell>
-                {leader.userAccount ? (
-                  <Badge variant="ACTIVE">
-                    <CheckIcon /> Account linked
-                  </Badge>
-                ) : (
-                  <Badge variant="INACTIVE">
-                    <XIcon />
-                    Unregistered
-                  </Badge>
-                )}
-              </TableCell>
-              <TableCell className="text-center">
-                {leader._count.disciples === 0 ? (
-                  <Badge variant="outline">No disciples yet</Badge>
-                ) : (
-                  <Badge variant="ACTIVE" className="normal-case">
-                    {leader._count.disciples}{" "}
-                    {pluralize("disciple", leader._count.disciples)}
-                  </Badge>
-                )}
-              </TableCell>
-              <TableCell className="text-center">
-                <LeaderRowActions leader={leader} />
-              </TableCell>
             </TableRow>
-          ))}
+          ) : (
+            leaders.map((leader, index) => (
+              <TableRow key={leader.id} className="hover:bg-transparent">
+                <TableCell>{index + 1}</TableCell>
+                <TableCell className="font-medium">
+                  <div>
+                    <Link
+                      href={`/leaders/${leader.id}`}
+                      className="hover:underline"
+                    >
+                      {leader.name}
+                    </Link>
+                    <p className="text-xs text-muted-foreground capitalize">
+                      {removeUnderscores(leader.memberType)},{" "}
+                      {leader.gender.toLowerCase()}
+                    </p>
+                  </div>
+                </TableCell>
+                <TableCell>
+                  {leader.userAccount ? (
+                    leader.userAccount.email
+                  ) : (
+                    <span className="text-muted-foreground">
+                      No account linked yet.
+                    </span>
+                  )}
+                </TableCell>
+                <TableCell>
+                  {leader.userAccount ? (
+                    <Badge variant="ACTIVE">
+                      <CheckIcon /> Account linked
+                    </Badge>
+                  ) : (
+                    <Badge variant="INACTIVE">
+                      <XIcon />
+                      Unregistered
+                    </Badge>
+                  )}
+                </TableCell>
+                <TableCell className="text-center">
+                  {leader._count.disciples === 0 ? (
+                    <Badge variant="outline">No disciples yet</Badge>
+                  ) : (
+                    <Badge variant="ACTIVE" className="normal-case">
+                      {leader._count.disciples}{" "}
+                      {pluralize("disciple", leader._count.disciples)}
+                    </Badge>
+                  )}
+                </TableCell>
+                <TableCell className="text-center">
+                  <LeaderRowActions leader={leader} />
+                </TableCell>
+              </TableRow>
+            ))
+          )}
         </TableBody>
       </Table>
     </div>

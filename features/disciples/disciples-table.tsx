195b3a94e116838bfc0c3183367df70ae1c99052
engrjeff <engineerjeff15@gmail.com>
2025-09-22 12:35:39 +0800

    fix height

--- features/disciples/disciples-table.tsx
+++ features/disciples/disciples-table.tsx
@@ -25,7 +25,7 @@ export function DisciplesTable({
   disciples: Array<Disciple & { leader: { id: string; name: string } | null }>;
 }) {
   return (
-    <div className="[&>div]:rounded-md rounded-md border min-h-min overflow-hidden lg:[&>div]:max-h-[500px]">
+    <div className="[&>div]:rounded-md rounded-md border min-h-min overflow-hidden lg:[&>div]:max-h-[490px]">
       <Table>
         <TableHeader className="bg-card sticky top-0 z-10 backdrop-blur-sm">
           <TableRow className="hover:bg-transparent">

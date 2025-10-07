09a5ba5976b9a836508b6fcf9532728b7c842fc6
engrjeff <engineerjeff15@gmail.com>
2025-10-07 10:21:40 +0800

    update cell report list item ui

--- features/cell-reports/cell-report-list.tsx
+++ features/cell-reports/cell-report-list.tsx
@@ -1,28 +1,22 @@
 "use client";
 
-import { ChevronRightIcon, PackageIcon } from "lucide-react";
-import { Badge } from "@/components/ui/badge";
-import { Button } from "@/components/ui/button";
+import {
+  BookOpenIcon,
+  CalendarIcon,
+  MapPinIcon,
+  PackageIcon,
+} from "lucide-react";
+import { CellType } from "@/app/generated/prisma";
 import {
   List,
   ListItem,
   ListItemContent,
+  ListItemLeading,
   ListItemPrimary,
   ListItemSecondary,
-  ListItemTrailing,
 } from "@/components/ui/list";
-import {
-  Sheet,
-  SheetClose,
-  SheetContent,
-  SheetDescription,
-  SheetHeader,
-  SheetTitle,
-  SheetTrigger,
-} from "@/components/ui/sheet";
 import type { CellReportRecord } from "@/types/globals";
-import { CellReportDetails } from "./cell-report-details";
-import { GeneratePDFButtonWide } from "./cell-report-pdf";
+import { CellReportRowActions } from "./cell-report-row-actions";
 
 export function CellReportList({
   cellReports,
@@ -49,81 +43,56 @@ export function CellReportList({
   );
 }
 
+function CellTypeIndicator({ cellType }: { cellType: CellType }) {
+  if (cellType === CellType.OPEN) {
+    return (
+      <span className="inline-block h-10 w-1 rounded-full bg-green-500"></span>
+    );
+  }
+
+  if (cellType === CellType.DISCIPLESHIP) {
+    return (
+      <span className="inline-block h-10 w-1 rounded-full bg-blue-500"></span>
+    );
+  }
+
+  if (cellType === CellType.SOULWINNING) {
+    return (
+      <span className="inline-block h-10 w-1 rounded-full bg-yellow-500"></span>
+    );
+  }
+}
+
 function CellReportItem({ cellReport }: { cellReport: CellReportRecord }) {
   return (
     <ListItem className="relative bg-card/40">
-      <Badge variant={cellReport.type} className="absolute top-1 right-1">
-        {cellReport.type}
-      </Badge>
+      <ListItemLeading>
+        <CellTypeIndicator cellType={cellReport.type} />
+      </ListItemLeading>
       <ListItemContent>
         <ListItemPrimary className="flex items-center gap-2 hover:underline">
-          Led by:{" "}
+          Led By:{" "}
           {cellReport.assistant
             ? cellReport.assistant.name
-            : cellReport.leader.name}
+            : cellReport.leader.name}{" "}
         </ListItemPrimary>
-
-        <ListItemSecondary>
-          Network Leader: {cellReport.leader?.name}
-        </ListItemSecondary>
-
-        <ListItemSecondary className="mt-3 text-foreground">
+        <ListItemSecondary className="flex items-center gap-2 truncate whitespace-nowrap text-xs">
+          <BookOpenIcon className="size-3 shrink-0" />{" "}
           {cellReport.lessonTitle
             ? cellReport.lessonTitle
-            : cellReport.lesson?.title}
-        </ListItemSecondary>
-        <ListItemSecondary>
+            : cellReport.lesson?.title}{" "}
+          &bull;{" "}
           {cellReport.hasCustomLesson
             ? "Custom Lesson"
             : cellReport.lesson?.lessonSeries?.title}
         </ListItemSecondary>
-
-        <ListItemSecondary className="mt-3 text-foreground">
-          {cellReport.venue}
+        <ListItemSecondary className="flex items-center gap-2 truncate whitespace-nowrap text-muted-foreground text-xs">
+          <MapPinIcon className="size-3 shrink-0" /> {cellReport.venue} &bull;{" "}
+          <CalendarIcon className="size-3 shrink-0" />
+          {cellReport.date}
         </ListItemSecondary>
-        <ListItemSecondary>{cellReport.date}</ListItemSecondary>
       </ListItemContent>
-      <ListItemTrailing onClick={() => {}}>
-        <Sheet>
-          <SheetTrigger asChild>
-            <Button
-              size="iconSm"
-              variant="ghost"
-              className="rounded-full"
-              aria-label="View Details"
-            >
-              <ChevronRightIcon />
-            </Button>
-          </SheetTrigger>
-          <SheetContent
-            side="right"
-            className="inset-y-2 right-2 flex h-auto w-[95%] flex-col gap-0 overflow-y-hidden rounded-lg border bg-background p-0 focus-visible:outline-none sm:max-w-lg"
-          >
-            <SheetHeader className="border-b p-4 text-left">
-              <SheetTitle>Cell Report Details</SheetTitle>
-              <div className="flex items-center justify-between">
-                <SheetDescription>
-                  Information about this cell report.
-                </SheetDescription>
-                <Badge variant={cellReport.type}>{cellReport.type}</Badge>
-              </div>
-            </SheetHeader>
-
-            <div className="max-h-[calc(100%-69px)] flex-1 overflow-y-auto pt-1 pb-4">
-              <CellReportDetails cellReport={cellReport} />
-            </div>
-
-            <div className="mt-auto flex justify-end gap-3 border-t p-4 text-right">
-              <SheetClose asChild>
-                <Button type="button" variant="outline" className="bg-muted/30">
-                  Close
-                </Button>
-              </SheetClose>
-              <GeneratePDFButtonWide cellReport={cellReport} />
-            </div>
-          </SheetContent>
-        </Sheet>
-      </ListItemTrailing>
+      <CellReportRowActions cellReport={cellReport} />
     </ListItem>
   );
 }

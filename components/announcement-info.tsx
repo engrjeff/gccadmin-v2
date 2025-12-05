3a88df2c5c2e0bce9690828b1a881f82b975a290
engrjeff <engineerjeff15@gmail.com>
2025-12-05 14:26:53 +0800

    refactor styles

--- components/announcement-info.tsx
+++ components/announcement-info.tsx
@@ -1,17 +1,18 @@
+import { ArrowRightIcon } from "lucide-react";
 import Link from "next/link";
+import { Button } from "./ui/button";
 
 export function AnnouncementInfo() {
   return (
-    <div className="flex items-center gap-2 border-b bg-secondary px-3 py-2">
+    <div className="gap-2 space-y-2 border-b bg-secondary px-3 py-2">
       <p className="text-sm">
         <span>🍀</span>Soul Winning & Consolidation Module is live!{" "}
       </p>
-      <Link
-        href="/soul-winning/new"
-        className="font-semibold text-blue-500 underline hover:no-underline"
-      >
-        Create Report &rarr;
-      </Link>
+      <Button size="sm" variant="link" className="px-0" asChild>
+        <Link href="/soul-winning/new">
+          Create Report <ArrowRightIcon />
+        </Link>
+      </Button>
     </div>
   );
 }

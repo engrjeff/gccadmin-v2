6e9ce8a659f28eac3f05b88e59462ba4d9fda001
engrjeff <engineerjeff15@gmail.com>
2025-12-05 14:17:46 +0800

    refactor AnnouncementInfo

--- components/announcement-info.tsx
+++ components/announcement-info.tsx
@@ -3,16 +3,15 @@ import Link from "next/link";
 export function AnnouncementInfo() {
   return (
     <div className="flex items-center gap-2 border-b bg-secondary px-3 py-2">
-      <span>🍀</span>
       <p className="text-sm">
-        Soul Winning & Consolidation Module is live!{" "}
-        <Link
-          href="/soul-winning/new"
-          className="text-green-500 underline hover:no-underline"
-        >
-          Create Report &rarr;
-        </Link>
+        <span>🍀</span>Soul Winning & Consolidation Module is live!{" "}
       </p>
+      <Link
+        href="/soul-winning/new"
+        className="font-semibold text-blue-500 underline hover:no-underline"
+      >
+        Create Report &rarr;
+      </Link>
     </div>
   );
 }
